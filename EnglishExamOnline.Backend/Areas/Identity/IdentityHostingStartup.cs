c602c5b3e820dc64b4afee1f999aba6f82cb9e82
Tuan Phuong <tuanphuong4139@gmail.com>
2021-04-01 18:30:37 +0700

    Add Scaffolding view

---
+++ EnglishExamOnline.Backend/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EnglishExamOnline.Backend.Data;
+using EnglishExamOnline.Backend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EnglishExamOnline.Backend.Areas.Identity.IdentityHostingStartup))]
+namespace EnglishExamOnline.Backend.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
