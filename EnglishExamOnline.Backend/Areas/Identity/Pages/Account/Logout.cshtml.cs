99eef38044fc010ec7601e05d20e22ac3ece575a
Tuan Phuong <tuanphuong4139@gmail.com>
2021-04-01 18:37:45 +0700

    Fix logout clinent site

--- EnglishExamOnline.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ EnglishExamOnline.Backend/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -8,29 +8,49 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using IdentityServer4.Services;
 
 namespace EnglishExamOnline.Backend.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
+        private readonly IIdentityServerInteractionService _interaction;
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger, IIdentityServerInteractionService interaction)
         {
+            _interaction = interaction;
             _signInManager = signInManager;
             _logger = logger;
         }
 
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
         {
+            [Required]
+            public string LogoutId { get; set; }
+        }
+
+        public void OnGet(string logoutid)
+        {
+            Input = new InputModel { LogoutId = logoutid };
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+            var logout = await _interaction.GetLogoutContextAsync(Input.LogoutId);
+
+            if (logout != null && !string.IsNullOrWhiteSpace(logout.PostLogoutRedirectUri))
+            {
+                return Redirect(logout.PostLogoutRedirectUri);
+            }
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
