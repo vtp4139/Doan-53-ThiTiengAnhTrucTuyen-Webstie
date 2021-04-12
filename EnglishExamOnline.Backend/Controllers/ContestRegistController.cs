4a16040af07a047266ccdbf537f5fec1913a4c69
Tuan Phuong <tuanphuong4139@gmail.com>
2021-04-12 23:56:50 +0700

    Update UI detail ( add modal comfirm). Add api post ContestRegist

---
+++ EnglishExamOnline.Backend/Controllers/ContestRegistController.cs
@@ -0,0 +1,49 @@
+﻿using EnglishExamOnline.Backend.Data;
+using EnglishExamOnline.Backend.Models;
+using EnglishExamOnline.Shared;
+using EnglishExamOnline.Shared.FormViewModels;
+using EnglishExamOnline.Shared.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnglishExamOnline.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class ContestRegistController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContestRegistController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ContestRegistVm>> PostQuest(ContestRegistFormVm createRequest)
+        {
+            var x = new ContestRegist
+            {
+                UserId = createRequest.UserId,
+                ContestId = createRequest.ContestId,
+                RegistDate = createRequest.RegistDate,
+            };
+
+            _context.ContestRegists.Add(x);
+            await _context.SaveChangesAsync();
+
+            return new ContestRegistVm
+            {
+                UserId = x.UserId,
+                ContestId = x.ContestId,
+                RegistDate = x.RegistDate,
+            });
+        }
+
+    }
+}
--- EnglishExamOnline.Shared/FormViewModels/ContestRegistFormVm.cs
+++ EnglishExamOnline.Shared/FormViewModels/ContestRegistFormVm.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EnglishExamOnline.Shared.ViewModels
+namespace EnglishExamOnline.Shared.FormViewModels
 {
-    public class ContestRegistVm
+    public class ContestRegistFormVm
     {
+        [Required]
         public int UserId { get; set; }
 
+        [Required]
         public int ContestId { get; set; }
 
-        public int ContestRegistId { get; set; }
-
         public DateTime RegistDate { get; set; }
+      
+        public virtual List<int> Results { get; set; }
     }
 }
